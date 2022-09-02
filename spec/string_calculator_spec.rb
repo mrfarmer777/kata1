601ddf8975852456b0910d0a0b96ac87964fddf3
Matt <mrfarmer777@gmail.com>
2022-09-01 21:34:20 -0500

    Handles receiving one number in the input string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -5,5 +5,11 @@ RSpec.describe StringCalculator do
     it "treats emptry string as a zero" do
         expect(subject.add("")).to eq(0)
     end
+
+    it "returns the number when only one is provided" do
+      expect(subject.add("1")).to eq(1)
+      expect(subject.add("500")).to eq(500)
+      expect(subject.add("123456")).to eq(123456)
+    end
   end
 end
