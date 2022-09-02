75ff666069502784e6f2b9bff4f91e33e5c60519
Matt <mrfarmer777@gmail.com>
2022-09-02 18:06:20 -0500

    Refactor delimiter parsing logic
    This change moves the parsing logic for the provided delimiters out
    to a separate method. I would have liked to do this sooner but
    was not sure if doing it is even in the spirit of the exercise.
    
    But alas.

--- app/string_calculator.rb
+++ app/string_calculator.rb
@@ -6,13 +6,7 @@ class StringCalculator
     delimeter_string = input_string.slice(DELIMETER_INPUT_REGEX)
         
     if delimeter_string
-      if delimeter_string.length > 1
-        delimeters = delimeter_string.scan(DELIMETER_PARSING_REGEX)
-        delimeters = delimeters.map{|d| Regexp.escape(d) }
-        delimeter = /#{delimeters.join("|")}/
-      else
-        delimeter = delimeter_string
-      end
+      delimeter = parse_provided_delimeter(delimeter_string)
       addend_string = input_string.split("\n")[1]
     else
       addend_string = input_string
@@ -21,7 +15,7 @@ class StringCalculator
       
     addends = addend_string.split(delimeter)    
 
-    addends.reduce(0) do |total, n| 
+    addends.reduce(0) do |total, n|
       number = n.to_i
       if number >= 0
         number <= 1000 ? total + number : total
@@ -29,5 +23,21 @@ class StringCalculator
         raise Exception.new("negatives not allowed")
       end
     end    
+  end
+
+  private
+
+  def parse_provided_delimeter(delimeter_string)
+    if delimeter_string.length > 1
+      compose_delimeter(delimeter_string)
+    else
+      delimeter_string
+    end
+  end
+
+  def compose_delimeter(delimeter_string)
+    delimeters = delimeter_string.scan(DELIMETER_PARSING_REGEX)
+    delimeters = delimeters.map{|d| Regexp.escape(d) }
+    /#{delimeters.join("|")}/
   end
 end
